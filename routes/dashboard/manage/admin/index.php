ffc9fb06e8c570a014e5d091dba759e294b82ce5
OblivionGrimes <matheussantos.00@outlook.com>
2026-07-17 11:08:55 -0300

    cadastros

--- routes/dashboard/manage/admin/index.php
+++ routes/dashboard/manage/admin/index.php
@@ -33,6 +33,48 @@
                         <span class="text-sm sidebar-text whitespace-nowrap transition-opacity duration-200">Relatórios</span>
                     </a>
                 </li>
+
+                <li>
+                    <div class="kt-menu kt-menu-default " data-kt-menu="true">
+                        <div class="kt-menu-item kt-menu-item-dropdown w-full"
+                            data-kt-menu-item-offset="0, 0"
+                            data-kt-menu-item-placement="bottom-start"
+                            data-kt-menu-item-toggle="dropdown"
+                            data-kt-menu-item-trigger="click">
+
+                            <button class="kt-menu-toggle flex items-center gap-2 px-3 py-2 rounded hover:bg-gray-100 w-full">
+                                <i class="ki-filled ki-additem fs-5 text-gray-500"></i>
+                                <span class="text-sm sidebar-text whitespace-nowrap transition-opacity duration-200">Cadastros</span>
+                                <i class="ki-filled ki-down text-xs ms-auto"></i>
+                            </button>
+
+                            <div class="kt-menu-dropdown py-2 kt-scrollable-y " style="sidebar-dropdown-width;">
+
+                                <div class="kt-menu-item ">
+                                    <a href="#" data-target="section-usuarios" class="sidebar-link flex items-center gap-2 px-3 py-2 rounded hover:bg-gray-100 group">
+                                        <!-- <i class="ki-outline ki-users fs-5 text-gray-500"></i> -->
+                                        <span class="text-sm sidebar-text whitespace-nowrap transition-opacity duration-200">Relatórios</span>
+                                    </a>
+                                </div>
+
+                                <div class="kt-menu-item active">
+                                    <a class="kt-menu-link" href="#">
+                                        <span class="kt-menu-title text-sm">Fevereiro, 2024</span>
+                                    </a>
+                                </div>
+
+                                <div class="kt-menu-item">
+                                    <a class="kt-menu-link" href="#">
+                                        <span class="kt-menu-title text-sm">Março, 2024</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </li>
+
             </ul>
         </div>
 
